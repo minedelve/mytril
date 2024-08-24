b6ef16ab0aa5ccea8467a80064dbb6953e428d40
Nycolaide <laurent.grimaldi@minedelve.com>
2024-08-24 13:40:55 +0200

    add consent mode and analytics on mytril.dev

---
+++ sites/mytril.dev/src/lib/analytics/modules/gtm.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	$: {
+		// @ts-ignore
+		if (typeof gtag !== 'undefined') {
+			// @ts-ignore
+			gtag('consent', 'default', {
+				ad_storage: 'denied',
+				analytics_storage: 'denied',
+				functionality_storage: 'denied'
+			});
+		}
+	}
+</script>
+
+<svelte:head>
+	<script async src={`https://www.googletagmanager.com/gtm.js?id=`}>
+	</script>
+</svelte:head>
+
+<!-- Google Tag Manager (noscript) -->
+<noscript>
+	<!-- svelte-ignore a11y-missing-attribute -->
+	<iframe
+		src="https://www.googletagmanager.com/ns.html?id="
+		height="0"
+		width="0"
+		style="display:none;visibility:hidden"
+	></iframe>
+</noscript>
+<!-- End Google Tag Manager (noscript) -->
--- sites/mytril.dev/src/routes/+layout.svelte
+++ sites/mytril.dev/src/routes/+layout.svelte
@@ -4,9 +4,15 @@
 	import '$lib/styles/doc.css';
 	import '../app.css';
 
+	import Tracking from '$lib/analytics/tracking.svelte';
+	import Rgpd from '$lib/components/rgpd.svelte';
 	import FooterCopyright from '$lib/components/footer-copyright.svelte';
 </script>
+
+<Tracking />
 
 <slot />
 
 <FooterCopyright />
+
+<Rgpd />
