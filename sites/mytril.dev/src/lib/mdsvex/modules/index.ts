6e426a95805ab14c380eac87f5995a2873292455
Nycolaide <laurent.grimaldi@minedelve.com>
2024-09-18 09:45:20 +0200

    clear repo

--- sites/mytril.dev/src/lib/mdsvex/modules/index.ts
+++
@@ -1,5 +0,0 @@
-import h1 from './h1.svelte';
-import h2 from './h2.svelte';
-import h3 from './h3.svelte';
-
-export { h1, h2, h3 };
--- sites/mytril.dev/src/lib/store/packages/state.ts
+++
@@ -1,8 +0,0 @@
-import { writable } from 'svelte/store';
-
-interface PackageVersion {
-	name: string;
-	version: string;
-}
-
-export const versions = writable<PackageVersion[]>([]);
