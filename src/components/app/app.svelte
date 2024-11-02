324a6299502fc05b0709af0d85cf68421a2d2291
Nycolaide <laurent.grimaldi@minedelve.com>
2024-11-02 23:23:32 +0100

    rewrite component App with svelte 5

--- src/components/app/app.svelte
+++ src/components/app/app.svelte
@@ -1,14 +1,17 @@
 <script lang="ts">
+	import type { Snippet } from 'svelte';
 	import { innerWidth } from '$lib/composables/display.js';
-	$: width = 0;
 
-	$: {
+	let { children }: { children: Snippet } = $props();
+	let width = $state(0);
+
+	$effect(() => {
 		if (width > 0) {
 			innerWidth.set(width);
 		}
-	}
+	});
 </script>
 
 <svelte:window bind:innerWidth={width} />
 
-<slot />
+{@render children?.()}
