120bd57db5cfa1df75715574e8b56f75d36ef072
Nycolaide <laurent.grimaldi@minedelve.com>
2024-11-04 23:41:14 +0100

    rewrite all component on svelte v4

--- src/components/app/app.svelte
+++ src/components/app/app.svelte
@@ -1,17 +1,16 @@
 <script lang="ts">
-	import type { Snippet } from 'svelte';
 	import { innerWidth } from '$lib/composables/display.js';
 
-	let { children }: { children: Snippet } = $props();
-	let width = $state(0);
+	// state
+	let width = 0;
 
-	$effect(() => {
+	$: {
 		if (width > 0) {
 			innerWidth.set(width);
 		}
-	});
+	}
 </script>
 
 <svelte:window bind:innerWidth={width} />
 
-{@render children?.()}
+<slot />
--- src/components/grids/spacer/spacer.svelte
+++ src/components/grids/spacer/spacer.svelte
@@ -1,20 +1,9 @@
 <script lang="ts">
 	import { className } from '$lib/utils/dom.js';
 
-	// let { class: className = '' } = $props();
-
-	// let { class }: { class: string | undefined } = $props();
-	// eslint-disable-next-line @typescript-eslint/no-explicit-any
-	// let { ...others }: any = $props();
-
-	let { class: _class, ...props }: any = $props();
+	// props
+	let _class: string | undefined = undefined;
+	export { _class as class };
 </script>
 
-<!-- let _class: string | false | undefined = undefined;
-	export { _class as class }; -->
-
-<!-- <div class:my-spacer={true}   {...props}>spacer</div> -->
-
-<!-- <div class={'my-spacer' + _class} {...props}>spacer</div> -->
-<!-- <div class="my-spacer {_class}" {...props}>spacer</div> -->
-<div class={className('my-spacer', _class)} {...props}>spacer</div>
+<div class={className('my-spacer', _class)} {...$$restProps}></div>
