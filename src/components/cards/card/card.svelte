db759218cfbc5d67966a4b89c77c61b43d6179dd
Nycolaide <laurent.grimaldi@minedelve.com>
2024-11-12 22:47:04 +0100

    WIP bugfix themelist not load on store mytril + WIP  new function for getColor on component mytril

--- src/components/cards/card/card.svelte
+++ src/components/cards/card/card.svelte
@@ -1,13 +1,46 @@
 <script lang="ts">
+	import { getColors } from '$lib/composables/colors.js';
 	import { className } from '$lib/utils/dom.js';
 
 	// props
 	let _class: string | undefined = undefined;
 	export { _class as class };
-	export let tag: string = 'div';
+	export let tag: 'a' | 'button' | 'div' = 'div';
+	export let dark: boolean = false;
+	export let light: boolean = false;
+	export let active: boolean = false;
+	export let outline: boolean = false;
+	export let text: boolean = false;
+	export let disabled: boolean = false;
+	export let color: string | undefined = undefined;
+
+	$: {
+		if ($$props.href) tag = 'a';
+	}
+
+	const colors = getColors(color);
+	$: {
+		console.log('resultat', $colors);
+	}
 </script>
 
-<svelte:element this={tag} class={className('myt-card', _class)} {...$$restProps}>
+<!-- svelte-ignore a11y_no_static_element_interactions -->
+<svelte:element
+	this={tag}
+	class={className('myt-card', _class)}
+	class:myt-card--active={active}
+	class:myt-card--outline={outline}
+	class:myt-card--text={text}
+	class:myt-card--disabled={disabled}
+	class:light
+	class:dark
+	disabled={$$props.disabled}
+	on:change
+	on:click
+	{...$$restProps}
+	{...$$restProps}
+	style={`--background-color: ${$colors};`}
+>
 	<!-- slot: default -->
 	<slot />
 	<!-- /slot: default -->
--- src/composables/colors.ts
+++ src/composables/colors.ts
@@ -1,4 +1,4 @@
-import { writable, type Writable } from 'svelte/store';
+import { derived, writable, type Writable } from 'svelte/store';
 import { _default } from './defaults.js';
 
 const colors: Writable<{ [key: string]: string | { [key: string]: string } }> = writable(
@@ -7,4 +7,35 @@ const colors: Writable<{ [key: string]: string | { [key: string]: string } }> = 
 
 export const setColors = (list: { [key: string]: string | { [key: string]: string } }) => {
 	colors.set(list);
+};
+
+const colorFormatRegex = /^(?:[a-zA-Z]+)(?:\d+|A\d+)$/;
+
+export const getColors = (color?: string) => {
+	if (color) {
+		return derived(colors, ($colors) => {
+			if ($colors[color]) {
+				// is a theme color
+				return `var(--${color})`;
+			} else if (color.includes('#') || color.includes('rgb') || color.includes('rgba')) {
+				// is a hexa/rgb/rgba color
+				return color;
+			} else if (colorFormatRegex.test(color)) {
+				const colorFormatRegex = /^([a-zA-Z]+)(\d+|A\d+)$/;
+				const match = color.match(colorFormatRegex);
+				// has material  / tailwind color
+				if (match) {
+					return `var(--${match[1]}-${match[2]})`;
+				} else {
+					return undefined;
+				}
+			} else if (color) {
+				return color;
+			} else {
+				return undefined;
+			}
+		});
+	} else {
+		return undefined;
+	}
 };
