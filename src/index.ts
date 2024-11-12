8282eccfa9692a36ff65c23185068af44d0565fc
Nycolaide <laurent.grimaldi@minedelve.com>
2024-11-12 18:52:42 +0100

    create component card

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,9 @@ export { default as App } from './components/app/app.svelte';
 export { default as Img } from './components/img/img.svelte';
 export { default as Responsive } from './components/responsive/responsive.svelte';
 
+// cards
+export { default as Card } from './components/cards/card/card.svelte';
+
 // grids
 export { default as Container } from './components/grids/container/container.svelte';
 export { default as Row } from './components/grids/row/row.svelte';
