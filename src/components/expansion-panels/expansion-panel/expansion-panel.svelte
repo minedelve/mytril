9822344e714005c004b059c60431681375592637
Nycolaide <laurent.grimaldi@minedelve.com>
2024-12-02 16:03:58 +0100

    Add new documentation + WIP expansion-panel

--- src/components/expansion-panels/expansion-panel/expansion-panel.svelte
+++ src/components/expansion-panels/expansion-panel/expansion-panel.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import Icon from '$lib/components/icon/icon.svelte';
 	import { className, styleName } from '$lib/utils/dom.js';
 	import { formatStyleProperties } from '$lib/utils/formater.js';
 
@@ -6,12 +7,17 @@
 	let _class: string | undefined = undefined;
 	let _style: string | undefined = undefined;
 	export { _class as class, _style as style };
+	export let open: boolean = false;
+	export let text: string | undefined = undefined;
 	export let tag: string = 'div';
 	export let dark: boolean = false;
 	export let light: boolean = false;
 	export let rounded: string | undefined = undefined;
 	export let color: string | undefined = undefined;
 	export let colorText: string | undefined = undefined;
+
+	import arrowUp from './icons/arrow-up.svg';
+	import arrowDown from './icons/arrow-down.svg';
 
 	$: styled = formatStyleProperties({
 		background: color,
@@ -26,14 +32,32 @@
 	style={styleName(styled, _style)}
 	class:light
 	class:dark
+	class:myt-expansion-panel--active={open}
 	{...$$restProps}
 >
-	<button class="myt-expansion-panel--title" aria-expanded="false" type="button">
-		<!-- slot: title -->
-		<slot name="title" />
-		<!-- /slot: title -->
+	<button
+		class="myt-expansion-panel--title"
+		aria-expanded={open}
+		type="button"
+		on:click={() => (open = open ? false : true)}
+	>
+		{#if $$slots.title}
+			<!-- slot: title -->
+			<slot name="title" />
+			<!-- /slot: title -->
+		{:else}
+			{text}
+		{/if}
+
+		<span class="myt-expansion-panel--icon">
+			{#if open}
+				<Icon icon={`svg:${arrowUp}`} />
+			{:else}
+				<Icon icon={`svg:${arrowDown}`} />
+			{/if}
+		</span>
 	</button>
-	<div class="myt-expansion-panel--text">
+	<div class="myt-expansion-panel--text" style={styleName(open ? undefined : 'display: none;')}>
 		<div class="myt-expansion-panel--text-wrapper">
 			<!-- slot: default -->
 			<slot />
--- src/components/expansion-panels/expansion-panels/expansion-panels.svelte
+++ src/components/expansion-panels/expansion-panels/expansion-panels.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { onDestroy } from 'svelte';
 	import { className, styleName } from '$lib/utils/dom.js';
 	import { formatStyleProperties } from '$lib/utils/formater.js';
 
@@ -13,6 +14,41 @@
 	export let color: string | undefined = undefined;
 	export let colorText: string | undefined = undefined;
 
+	let ref: HTMLElement | null = null;
+	let observer: MutationObserver | null = null;
+	let expansionPanels: HTMLElement[] = [];
+
+	$: {
+		if (ref) {
+			expansionPanels = Array.from(
+				ref.querySelectorAll(':scope > .myt-expansion-panel')
+			) as HTMLElement[];
+
+			if (observer) {
+				observer.disconnect();
+			}
+
+			observer = new MutationObserver((mutations) => {
+				for (const mutation of mutations) {
+					if (mutation.type === 'attributes' && mutation.attributeName === 'class') {
+						const target = mutation.target as HTMLElement;
+						if (target.classList.contains('myt-expansion-panel--active')) {
+							console.log('Mise à jour détectée: ', target);
+						}
+					}
+				}
+			});
+
+			expansionPanels.forEach((panel) => {
+				observer?.observe(panel, { attributes: true });
+			});
+		}
+	}
+
+	onDestroy(() => {
+		observer?.disconnect();
+	});
+
 	$: styled = formatStyleProperties({
 		background: color,
 		color: colorText,
@@ -22,6 +58,7 @@
 
 <svelte:element
 	this={tag}
+	bind:this={ref}
 	class={className('myt-expansion-panels', _class)}
 	style={styleName(styled, _style)}
 	class:light
